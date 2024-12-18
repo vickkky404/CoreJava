76efc71e63372410d16870cf73839fd8eaeecb03
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2024-12-18 19:55:51 +0530

    Add files via upload

---
+++ DT1.java
@@ -0,0 +1,30 @@
+public class DT1{
+	public static void main(String[] args){
+		//int datatype
+		int a ,b ;
+		a=10;
+		b=20;
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a % b);
+		System.out.println(a * b);
+		
+		String name = "HELLO WORLD";
+		//this is string datatype..
+		System.out.println(name);
+		
+		float weight = 22.4f;
+		//decimal value.
+		System.out.println(weight);
+		
+		boolean Student = true;
+		//true or false..
+		//system.out.println(isStudent);
+		
+		char initial = 'A' ;
+		char initial_2 ='B';
+		//character store.
+		System.out.println(initial +   initial_2);
+		
+	}
+}
---
+++ Vari_1.java
@@ -0,0 +1,8 @@
+public class Vari_1{
+	public static void main (String[] args){
+		int a=10;
+		int b=20;
+		
+		System.out.println("Hello World..." + a + b );
+	}
+}
