51bed5446e88ccaf4bd66ee34bf496c2d06f210a
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2024-12-31 22:04:15 +0530

    Add files via upload

--- CheckIfPriime.java
+++ CheckIfPriime.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class CheckIfPriime {
-    public static void main(String[] args) {
+// public class CheckIfPriime {
+//     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = sc.nextInt();
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         int number = sc.nextInt();
 
-        if(isPrime(number)){
-            System.out.println(number + "is a prime number ");
-        }else{
-            System.out.println();
-        }
-    }
-}
+//         if(isPrime(number)){
+//             System.out.println(number + "is a prime number ");
+//         }else{
+//             System.out.println();
+//         }
+//     }
+// }
