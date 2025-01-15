be8f41a78ce2c69812f960b61ae5dd3920401bd5
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-15 23:09:11 +0530

    Add files via upload

---
+++ BasicOperator.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 5;
+console.log(a + b); //adddition
+console.log(a - b); //subtraction
+console.log(a * b); //multiplication..
+console.log(a / b); //division..
+console.log(a % b); //modulus..
