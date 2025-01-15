acc3df036f12dec6d3e0f37a336d50f71bede0de
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-15 23:09:34 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,2 @@
+// JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.
+console.log("Hello World")
