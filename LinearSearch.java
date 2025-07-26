507638735705380da474cfbb0a6d4f0d53bc25da
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-26 22:21:23 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,24 @@
+
+import java.lang.reflect.Array;
+
+
+
+public class LinearSearch{
+    public static int test(int arr[] , int key){
+        for(int i = 0; i< arr.length ; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        // linear Search
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int key = 8;
+
+
+         
+    }
+}
---
+++ OddIndex.java
@@ -0,0 +1,16 @@
+public class OddIndex {
+    public static void main(String[] args){
+
+        // creating array
+        int arr[] = {20,30,40,50,60};
+        
+
+        // creating loop 
+        for(int i = 0 ; i<arr.length ; i++){{
+            if(ki)
+        }
+
+        }
+    }
+    
+}
