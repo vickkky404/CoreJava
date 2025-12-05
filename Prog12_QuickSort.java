2764eca378771ba52a569bbc4d486b8fab712724
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-05 13:37:08 +0530

    Add Prog12_QuickSort.java

---
+++ Prog12_QuickSort.java
@@ -0,0 +1,39 @@
+public class Prog12_QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        
+        return i + 1;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array: ");
+        for (int num : arr) System.out.print(num + " ");
+        
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("\nSorted array: ");
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
