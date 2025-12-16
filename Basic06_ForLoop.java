4e20324df0fb001efc2f90be1d5b6eb6ac566083
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:54:05 +0530

    Create Basic06_ForLoop.java

---
+++ Basic06_ForLoop.java
@@ -0,0 +1,8 @@
+public class Basic06_ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Numbers from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
