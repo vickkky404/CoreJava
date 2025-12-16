925d75e4269b2d2fbceb47b04bde023265fc6e77
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:57:02 +0530

    Create Basic10_StringLength.java

---
+++ Basic10_StringLength.java
@@ -0,0 +1,9 @@
+public class Basic10_StringLength {
+    public static void main(String[] args) {
+        String str = "Hello Java";
+        System.out.println("String: " + str);
+        System.out.println("Length: " + str.length());
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+    }
+}
