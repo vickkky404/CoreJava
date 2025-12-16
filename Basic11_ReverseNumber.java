110e4164dc2e67988821e5fbff96c68ea5f4500f
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:58:16 +0530

    Add Basic11_ReverseNumber.java

---
+++ Basic11_ReverseNumber.java
@@ -0,0 +1,10 @@
+public class Basic11_ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12345, reversed = 0;
+        while (num != 0) {
+            reversed = reversed * 10 + num % 10;
+            num = num / 10;
+        }
+        System.out.println("Reversed: " + reversed);
+    }
+}
