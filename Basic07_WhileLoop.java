f1d364a526f9253ec5333e74a220159f6f88e839
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:54:34 +0530

    Add Basic07_WhileLoop.java

---
+++ Basic07_WhileLoop.java
@@ -0,0 +1,10 @@
+public class Basic07_WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Count using while loop:");
+        while (i <= 5) {
+            System.out.println("Count: " + i);
+            i++;
+        }
+    }
+}
