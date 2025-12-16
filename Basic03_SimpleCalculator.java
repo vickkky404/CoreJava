0069ea2545eb1f575d275210bf29ac45210b64e7
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:52:45 +0530

    Add Basic03_SimpleCalculator.java

---
+++ Basic03_SimpleCalculator.java
@@ -0,0 +1,10 @@
+public class Basic03_SimpleCalculator {
+    public static void main(String[] args) {
+        int a = 15, b = 10;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
