ce18d011e0337c2164aacbe404cb7c8d7e344228
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:53:40 +0530

    Add Basic05_LargestNumber.java

---
+++ Basic05_LargestNumber.java
@@ -0,0 +1,11 @@
+public class Basic05_LargestNumber {
+    public static void main(String[] args) {
+        int a = 50, b = 30, c = 40;
+        if (a > b && a > c)
+            System.out.println(a + " is the greatest");
+        else if (b > a && b > c)
+            System.out.println(b + " is the greatest");
+        else
+            System.out.println(c + " is the greatest");
+    }
+}
