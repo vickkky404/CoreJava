227d25fa3a8e2311d1b64c9d985ecaf418c8b5cc
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-16 15:53:10 +0530

    Add Basic04_EvenOddCheck.java

---
+++ Basic04_EvenOddCheck.java
@@ -0,0 +1,9 @@
+public class Basic04_EvenOddCheck {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num % 2 == 0)
+            System.out.println(num + " is Even");
+        else
+            System.out.println(num + " is Odd");
+    }
+}
