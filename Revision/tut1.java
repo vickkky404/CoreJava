4e6e8f2d9f33a28d6f21659ca823a3737c3349cb
nalin <nalinikantbastia@gmail.com>
2026-02-01 20:52:38 +0530

    loops

--- Revision/tut1.java
+++ Revision/tut1.java
@@ -13,7 +13,30 @@ public class tut1{
             System.out.println("i = " + i);
         }
 
+        System.out.println("-------------------");
         
-        
+        // for loop
+        for(int i = 0; i <= 5; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("-------------------");
+
+        // while Loop
+        int j = 0;
+        while(j <= 5){
+            System.out.println(j);
+            j++;
+        }
+
+        System.out.println("-------------------");
+
+        // do-while loop
+        int k = 1;
+        do{
+            System.out.println(k);
+            k++;
+        } while(k <= 5);
+
     }
 }
