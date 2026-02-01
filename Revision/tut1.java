abe685c01ac98ff3867a2a7300c7645a7f36f69c
nalin <nalinikantbastia@gmail.com>
2026-02-01 20:42:37 +0530

    Java Revise

---
+++ Revision/tut1.java
@@ -0,0 +1,9 @@
+package Revision;
+
+public class tut1{
+    public static void main(String[] args){
+
+        // printing
+        System.out.println("java Practice");
+    }
+}
--- first.js
+++
@@ -1,2 +0,0 @@
-// JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.
-console.log("Hello World")
