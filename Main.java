9061c5de8f8519c8917439051e432bed00971489
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-02-07 20:19:27 +0530

    Implement main method to print 'Hello World'
    Fix syntax error in print statement.

---
+++ Main.java
@@ -0,0 +1,8 @@
+// public= makes it access everywhere
+public class Main{
+// main function , common in every program
+  public static void main(String[] args){
+    System.out.println("Hello World);
+  }
+  
+}
