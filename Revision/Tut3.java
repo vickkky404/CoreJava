9f46901d2ddcf00c0317258a18a19a5c5a45684d
nalin <nalinikantbastia@gmail.com>
2026-02-07 11:59:30 +0530

    Loop

---
+++ Revision/Tut3.java
@@ -0,0 +1,12 @@
+package Revision;
+// Print numbers 1 to 100
+// using for loop
+
+public class Tut3 {
+    public static void main(String[] args){
+
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
