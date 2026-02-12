2473f4f06cb674a69ced60894689b95db4559f8b
nalin <nalinikantbastia@gmail.com>
2026-02-12 16:41:24 +0530

    ReTouch Java

---
+++ Revision/Loops/If_Else.java
@@ -0,0 +1,18 @@
+package Revision.Loops;
+
+public class If_Else {
+    public static void main(String[] agrs){
+
+        int salary = 25400;
+        if(salary > 10000){
+            salary = salary + 2000;
+
+        }
+        else{
+            salary = salary + 1000;
+        }
+
+        System.out.println(salary);
+
+    }
+}
