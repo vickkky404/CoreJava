0273da0f7e9386bab74918743242dad3308090eb
nalin <nalinikantbastia@gmail.com>
2026-02-20 10:35:38 +0530

    basics of input taking

--- Revision/Main.java
+++ Revision/Main.java
@@ -1,9 +1,11 @@
 package Revision;
+import java.util.Scanner;
 
 public class Main {
 
 //    this  is the main function.
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello World");
     }
 }
