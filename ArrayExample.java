6ba47707a29608324b6d35e823b6edb9425a17bc
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-02-22 17:48:28 +0530

    Add ArrayExample class to demonstrate array usage

---
+++ ArrayExample.java
@@ -0,0 +1,11 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + " = " + numbers[i]);
+        }
+    }
+}
+
