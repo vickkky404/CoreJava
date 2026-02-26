357dc66e35430d09f67b492c2bf1f0290026774f
nalin <nalinikantbastia@gmail.com>
2026-02-26 16:42:13 +0530

    Logic Building

---
+++ Revision/SumOfArray.java
@@ -0,0 +1,14 @@
+package Revision;
+import java.util.*;
+
+public class SumOfArray {
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        int sum = 0;
+
+        for(int num : arr){
+            sum += num;
+        }
+        System.out.println("Sum of the Array is: ");
+    }
+}
