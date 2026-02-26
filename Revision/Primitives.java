bc74114d5354d1721ff88d7a81c9a832c1ee8ffb
nalin <nalinikantbastia@gmail.com>
2026-02-26 16:41:52 +0530

    Logic Building

---
+++ Revision/Primitives.java
@@ -0,0 +1,12 @@
+package Revision;
+
+public class Primitives {
+    int rollno = 64;
+//    the last data type which we cant divide further is called as primitives
+
+//    string is not a primitive
+    String name = "Nalinikant";
+    float marks = 98.67f;
+
+
+}
