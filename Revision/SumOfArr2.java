a40e14897cea41475d717380e6778a193a6978ca
nalin <nalinikantbastia@gmail.com>
2026-02-26 16:42:00 +0530

    Logic Building

---
+++ Revision/SumOfArr2.java
@@ -0,0 +1,14 @@
+package Revision;
+
+public class SumOfArr2 {
+    public static void main(String[] args){
+
+        int[] arr = {10,20,30,40,50};
+        int sum = 0;
+
+       for(int i = 0; i < arr. sum += arr[i]);
+
+
+        }
+    }
+}
